78c3a56ca8d87c26ed24537401d39004fdd1b030
Kelvin Chen <kelvgpd@gmail.com>
2023-08-05 22:23:02 -0400

    rebuild javadoc

--- Implementation/Javadoc/package-search-index.js
+++ Implementation/Javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controllers"},{"l":"data"},{"l":"data.databases"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controllers"},{"l":"data"},{"l":"data.databases"},{"l":"views"}];updateSearchResults();
